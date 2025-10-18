e7d35080ab6e95a9fbac5a9f97ddb383d0939548
mabusall <mabusall.developer@gmail.com>
2025-10-18 17:52:09 +0300

    backup database

---
+++ Solution Items/sync inquiries.sql
@@ -0,0 +1,24 @@
+SELECT * FROM public."ContentSyncLog"
+ORDER BY "Id" ASC;
+
+SELECT * FROM public."DailyApiUsage"
+ORDER BY "Id" ASC;
+
+SELECT * FROM public."Content"
+ORDER BY "Id" ASC;
+
+SELECT * FROM public."ContentCrew"
+ORDER BY "Id" ASC;
+
+SELECT * FROM public."ContentCast"
+ORDER BY "Id" ASC;
+
+SELECT * FROM public."ContentImage"
+ORDER BY "Id" ASC;
+
+
+SELECT * FROM public."Genre"
+ORDER BY "Id" ASC;
+
+SELECT * FROM public."Person"
+ORDER BY "Id" ASC;
